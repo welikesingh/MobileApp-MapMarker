48a763a7fc7ba1d8d965360e23df856f9eb541c8
welikesingh <welikesingh@gmail.com>
2025-11-30 12:30:22 -0600

    this is first change

---
+++ 05_Map_Service_and_Location/models/place.js
@@ -0,0 +1,23 @@
+/*
+export class Place {
+  constructor(title, imageUri, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = location.address;
+    this.location = { lat: location.lat, lng: location.lng }; // { lat: 0.141241, lng: 127.121 }
+    this.id = new Date().toString() + Math.random().toString();
+  }
+}
+*/
+export class Place {
+  constructor(title, imageUri, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = location?.address ?? "No address"; // ✔ Safe
+    this.location = location
+      ? { lat: location.lat, lng: location.lng }
+      : { lat: null, lng: null };                     // ✔ Safe
+    this.id = new Date().toString() + Math.random().toString();
+  }
+}
+
